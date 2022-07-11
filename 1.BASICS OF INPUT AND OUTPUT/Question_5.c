e463fa783f39754973e2087f0d631fea527d90b4
Ayantika2208 <108997569+Ayantika2208@users.noreply.github.com>
2022-07-11 23:51:33 +0530

    Add files via upload

---
+++ 1.BASICS OF INPUT AND OUTPUT/Question_5.c
@@ -0,0 +1,8 @@
+//WAP to calculate the length of String using printf function.
+#include<stdio.h>
+int main()
+{
+    int n = printf("AYANTIKA");
+    printf("\n%d",n);
+    return 0;
+}
---
+++ 1.BASICS OF INPUT AND OUTPUT/Question_7.c
@@ -0,0 +1,15 @@
+//WAP to print “%d” on the screen.
+#include<stdio.h>
+int main()
+{
+    //method 1
+    /*char c ,b;
+    c='%';
+    b='d';
+    printf("%c%c",c,b);
+     return 0; */  
+
+    //method 2
+    printf("%%d");
+    return 0;
+}
